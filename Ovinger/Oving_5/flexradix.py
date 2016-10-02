4cb33c37e8fac3e4c1d9fcf64d3ef5b5dedba877
ohodegaa <olehakon95@dhcp-10-24-19-236.wlan.ntnu.no>
2016-10-02 18:05:31 +0200

    sort_by_index

--- Ovinger/Oving_5/flexradix.py
+++ Ovinger/Oving_5/flexradix.py
@@ -9,14 +9,23 @@ from collections import defaultdict
 
 
 def flexradix(A, d, i):
-    sorted_list = [""]*len(A)
-    while i <= d:
-        for word in A:
+
 
 
     return A
 
+def sort_by_index(A, i):
+    sorted_list = []
+    partitions = {}
+    for word in A:
+        if i >= len(word):
+            sorted_list.append(word)
+        elif partitions.get(word[i]) is None:
+            partitions[word[i]] = [word]
+        else:
+            partitions[word[i]].append(word)
 
+    return [words for words in partitions.values()]
 
 def main():
     stdin = open("input_eksempel_01.txt", "r+")
