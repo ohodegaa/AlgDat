6f2c2ac183a5ee4b6d80b6865dc5982808466661
ohodegaa <ohodegaa@stud.ntnu.no>
2016-10-15 21:38:09 +0200

    Implementert 'minimum_size' som finner minste bredde/hoyde

--- Ovinger/Oving_7/seddeltrykkeriet.py
+++ Ovinger/Oving_7/seddeltrykkeriet.py
@@ -6,21 +6,56 @@ from sys import stdin
 
 def max_value(widths, heights, values, paper_width, paper_height):
     # SKRIV DIN KODE HER
-    return max_value_greedy(widths, heights, values, paper_width, paper_height)
+
+    # result 2-D - array
+    # accessing table with result[width][height] according to coordinate system
+    result = [[]] * (paper_width + 1)
+    for i in range(paper_width + 1):
+        result[i] = [-1]*(paper_height + 1)
+
+    # zeroing out width=0 and height=0
+    for i in range(len(result)):
+        result[i][0] = 0
+    for j in range(len(result[0])):
+        result[0][j] = 0
+
+    # zeroing out the less-than-minimum-sized note. No note will be smaller than min_size
+    min_size = minimum_size(widths, heights)
+    for i in range(1, min_size):
+        # rows:
+        for r in range(1, paper_width + 1):
+            result[r][i] = 0
+        for c in range(1, paper_height + 1):
+            result[i][c] = 0
+
+    return max_value_dynamic(widths, heights, values, paper_width, paper_height)
+
+
+def minimum_size(widths, heights):
+    min_size = 10**6
+    for i in range(len(widths)):
+        if widths[i] < min_size:
+            min_size = widths[i]
+        if heights[i] < min_size:
+            min_size = heights[i]
+    return min_size
+
 
 def max_value_greedy(widths, heights, values, paper_width, paper_height):
     pass
+
 
 def max_value_dynamic(widths, heights, values, paper_width, paper_height):
     max_value = 0
 
     # 2-D array representing the whole sheet [width][height]
-    result = [None]*paper_width
-    for i in range(len(result)):
-        result[i] = [-1]*paper_height
+    pass
 
-
-
+def show_table(c):
+    for i in range(len(c[0])):
+        for j in range(len(c)):
+            print(c[j][i], end="\t")
+        print()
 
 
 def main():
