0148ce2a06d5341e5c76bf98967e5ca51a165122
ohodegaa <ohodegaa@stud.ntnu.no>
2016-10-19 10:28:24 +0200

    Implemented min_coins_greedy

--- Ovinger/Oving_8/pengeveksling.py
+++ Ovinger/Oving_8/pengeveksling.py
@@ -6,17 +6,31 @@ Inf = 1000000000
 
 
 def min_coins_greedy(coins, value):
+    num_of_coins = 0
+    amount = 0
+    i = 0
+    while amount != value:
+        if amount + coins[i] <= value:
+            amount += coins[i]
+            num_of_coins += 1
+            if amount + coins[i] > value:
+                i += 1
+        else:
+            i += 1
 
+    return num_of_coins
 
 
 def min_coins_dynamic(coins, value):
     # SKRIV DIN KODE HER
+    pass
 
 
 def can_use_greedy(coins):
     # bare returner False her hvis du ikke klarer aa finne ut
     # hva som er kriteriet for at den graadige algoritmen skal fungere
     # SKRIV DIN KODE HER
+    pass
 
 coins = []
 for c in stdin.readline().split():
