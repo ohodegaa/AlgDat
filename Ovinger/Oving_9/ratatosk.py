fee2c23f5203bc0c06974f770de75fbe57cf9056
ohodegaa <ohodegaa@stud.ntnu.no>
2016-10-24 19:10:19 +0200

    implemented bfs

--- Ovinger/Oving_9/ratatosk.py
+++ Ovinger/Oving_9/ratatosk.py
@@ -1,8 +1,12 @@
 __author__ = 'ohodegaa'
 
+from queue import deque
 #!/usr/bin/python3
 
 from sys import stdin
+
+
+
 
 
 class Node:
@@ -10,15 +14,51 @@ class Node:
         self.child = []
         self.ratatosk = False
         self.next_child = 0
+        self.dist = 0
+
+    def __repr__(self):
+        return "Dist: " + str(self.dist) + ", " + str(self.ratatosk)
 
 
 def dfs(root):
-    # SKRIV DIN KODE HER
+    q = deque([])
+    if root.ratatosk:
+        return 0
+    root.next_child = 0
+    q.append(root)
+
+    while len(q) > 0:
+        node = q.pop()
+        if node.ratatosk:
+            return node.dist
+        if node.next_child < len(node.child):
+            node.child[node.next_child].dist = node.dist + 1
+            q.append(node.child[node.next_child])
 
 
-def bfs(root):
-    # SKRIV DIN KODE HER
 
+
+
+def bfs(root: Node):
+    q = deque([])
+    if root.ratatosk:
+        return 0
+    q.append(root)
+
+    while len(q) > 0:
+        node = q.popleft()
+        for child in node.child:
+            child.dist = node.dist + 1
+            if child.ratatosk:
+                return child.dist
+            q.append(child)
+
+
+
+
+
+
+stdin = open("input_eksempel_01.txt", "r+")
 function = stdin.readline().strip()
 number_of_nodes = int(stdin.readline())
 nodes = []
@@ -37,5 +77,7 @@ if function == 'dfs':
     print(dfs(start_node))
 elif function == 'bfs':
     print(bfs(start_node))
+
 elif function == 'velg':
     # SKRIV DIN KODE HER
+    pass
