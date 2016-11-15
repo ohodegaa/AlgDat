dd6463141625c24a7557466156c4f1e73754cefb
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-15 12:53:16 +0100

    Implemented Dijkstra

--- Ovinger/Oving_11/mumien.py
+++ Ovinger/Oving_11/mumien.py
@@ -2,14 +2,17 @@ __author__ = 'ohodegaa'
 
 from sys import stdin, stderr
 
+
 def best_path(nm, prob):
     return dijkstra(nm, prob)
 
+
 def dijkstra(nm, prob):
     n = len(nm)
-    visited = [False]*n
-    ancestors = [-1]*n
-    k_prob = [prob[0]] + [0.0]*(n-1)
+    visited = [False] * n
+    ancestors = [-1] * n
+    k_prob = [0.0] * n
+    k_prob[0] = prob[0]
     next_node = 0
     for i in range(n):
         node = next_node
@@ -17,51 +20,24 @@ def dijkstra(nm, prob):
         maxi_prob = -1.0
         for adj in range(n):
             if not visited[adj]:
-                if nm[node][adj]*k_prob[node]*prob[adj] > k_prob[adj]:
-                    k_prob[adj] = k_prob[node]*prob[adj]
+                if nm[node][adj] * k_prob[node] * prob[adj] > k_prob[adj]:
+                    k_prob[adj] = k_prob[node] * prob[adj]
                     ancestors[adj] = node
-                    if k_prob[adj] > maxi_prob:
-                        next_node = adj
-                        maxi_prob = k_prob[adj]
-
+                if k_prob[adj] > maxi_prob:
+                    next_node = adj
+                    maxi_prob = k_prob[adj]
         if next_node == n - 1:
             break
-    anc = n - 1
-    s = str(n - 1)
-    while ancestors[anc] != -1:
-        s = str(ancestors[anc]) + "-" + s
-        anc = ancestors[anc]
-    return s
-
-def floyd_warshall(nm, prob):
-    ancestors = []
-
-    for i in range(len(nm)):
-        neighbours = []
-        for j in range(len(nm[i])):
-            if nm[i][j]:
-                neighbours.append(i)
-                nm[i][j] = prob[i] * prob[j]
-            else:
-                neighbours.append(None)
-        ancestors.append(neighbours)
-
-    for k in range(len(nm)):
-        for i in range(len(nm)):
-            for j in range(len(nm[i])):
-                if nm[i][k] * nm[k][j] > nm[i][j]:
-                    nm[i][j] = nm[i][k] * nm[k][j]
-                    ancestors[i][j] = ancestors[k][j]
-    return print_path(ancestors, 0, len(nm) - 1, "")
+    return print_path(ancestors, 0, n - 1)
 
 
-def print_path(ancestors, i, j, s):
+def print_path(ancestors, i, j, s=""):
     if i == j:
-        return s + str(i)
-    elif ancestors[i][j] is None:
+        return s + str(j)
+    elif ancestors[j] == -1:
         return "0"
     else:
-        return print_path(ancestors, i, ancestors[i][j], s) + s + "-" + str(j)
+        return print_path(ancestors, i, ancestors[j], s) + "-" + str(j)
 
 #stdin = open("input_eksempel_01.txt")
 n = int(stdin.readline())
--- Ovinger/Oving_12/alletilalle.py
+++ Ovinger/Oving_12/alletilalle.py
@@ -22,3 +22,48 @@ def best_path(nm, prob):
                     ancestors[i][j] = ancestors[k][j]
     return print_path(ancestors, 0, len(nm) - 1, "")
 """
+def print_path(ancestors, i, j, s):
+    if i == j:
+        return s + str(i)
+    elif ancestors[i][j] is None:
+        return "0"
+    else:
+        return print_path(ancestors, i, ancestors[i][j], s) + s + "-" + str(j)
+
+
+
+
+def print_D(D, k):
+    print("D("+str(k)+")")
+    for i in range(len(D)):
+        print(D[i])
+
+def floyd_warshall(nm):
+    n = len(nm)
+    D = [] + nm
+    ancestors = []
+    for i in range(len(nm)):
+        neighbours = []
+        for j in range(len(nm[i])):
+            if nm[i][j] < 90:
+                neighbours.append(i)
+            else:
+                neighbours.append(None)
+        ancestors.append(neighbours)
+    for k in range(n):
+        print_D(D, k)
+        for i in range(n):
+            for j in range(n):
+                if D[i][j] > D[i][k] + D[k][j]:
+                    D[i][j] = D[i][k] + D[k][j]
+                    ancestors[i][j] = ancestors[k][j]
+    print(print_path(ancestors, 0, 3, ""))
+
+
+inf = 100
+floyd_warshall([[0, 2, inf, inf, inf, inf],
+                [inf, 0, 1, inf, 4, inf],
+                [-2, inf, 0, inf, 2, inf],
+                [inf, 4, inf, 0, inf, 3],
+                [inf, inf, inf, 1, 0, 2],
+               [inf, inf, inf, inf, inf, 0]])
