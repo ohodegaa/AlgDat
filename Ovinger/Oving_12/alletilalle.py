c10388537c1c8f1adc49ebb6ca29fd80a1afed07
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-15 21:25:55 +0100

    Oving_12

--- Ovinger/Oving_12/alletilalle.py
+++ Ovinger/Oving_12/alletilalle.py
@@ -1,69 +1,72 @@
 __author__ = 'ohodegaa'
 
-"""
-def best_path(nm, prob):
-    ancestors = []
-
-    for i in range(len(nm)):
-        neighbours = []
-        for j in range(len(nm[i])):
-            if nm[i][j]:
-                neighbours.append(i)
-                nm[i][j] = prob[i] * prob[j]
-            else:
-                neighbours.append(None)
-        ancestors.append(neighbours)
-
-    for k in range(len(nm)):
-        for i in range(len(nm)):
-            for j in range(len(nm[i])):
-                if nm[i][k] * nm[k][j] > nm[i][j]:
-                    nm[i][j] = nm[i][k] * nm[k][j]
-                    ancestors[i][j] = ancestors[k][j]
-    return print_path(ancestors, 0, len(nm) - 1, "")
-"""
-def print_path(ancestors, i, j, s):
-    if i == j:
-        return s + str(i)
-    elif ancestors[i][j] is None:
-        return "0"
-    else:
-        return print_path(ancestors, i, ancestors[i][j], s) + s + "-" + str(j)
-
-
-
-
-def print_D(D, k):
-    print("D("+str(k)+")")
-    for i in range(len(D)):
-        print(D[i])
-
-def floyd_warshall(nm):
-    n = len(nm)
-    D = [] + nm
-    ancestors = []
-    for i in range(len(nm)):
-        neighbours = []
-        for j in range(len(nm[i])):
-            if nm[i][j] < 90:
-                neighbours.append(i)
-            else:
-                neighbours.append(None)
-        ancestors.append(neighbours)
-    for k in range(n):
-        print_D(D, k)
-        for i in range(n):
-            for j in range(n):
-                if D[i][j] > D[i][k] + D[k][j]:
-                    D[i][j] = D[i][k] + D[k][j]
-                    ancestors[i][j] = ancestors[k][j]
-    print(print_path(ancestors, 0, 3, ""))
-
+from sys import stdin, maxsize
 
 inf = 100
-floyd_warshall([[0, 2, inf, inf, inf, inf],
-                [inf, 0, 1, inf, 4, inf],
-                [-2, inf, 0, inf, 2, inf],
-                [inf, 4, inf, 0, inf, 3],
-                [inf, inf, inf, 1, 0, 2],
-               [inf, inf, inf, inf, inf, 0]])
+
+
+def shortest_path(order_list, am, cities):
+    """
+    Greia her er at vi må finne shortest path fra order_list[0] til order_list[1],
+    fra order_list[1] til order_list[2] osv. og deretter shortest path fra
+    order_list[-1] til order_list[0]
+
+    Vi finner først shortest path fra alle til alle vha. Floyd-Warshall
+
+    :param order_list: rekkefølgen byene skal besøkes i
+    :param am: nabo-matrise
+    :param cities: totalt antall byer
+    :return: shortest path fra order_list[0] og tilbake til order_list[0], innom hver by i order_list
+
+    """
+    ancestors = []
+    for i in range(cities):
+        ciblings = []
+        for j in range(cities):
+            if am[i][j] > 0:
+                ciblings.append(i)
+            else:
+                ciblings.append(None)
+        ancestors.append(ciblings)
+
+    for k in range(cities):
+        for i in range(cities):
+            for j in range(cities):
+                if am[i][k] > 0 and am[k][j] > 0:
+                    if am[i][k] + am[k][j] < am[i][j]:
+                        am[i][j] = am[i][k] + am[k][j]
+                        ancestors[i][j] = ancestors[k][j]
+
+    return cost(ancestors, am, order_list + [order_list[0]])
+
+
+def cost(ancestors, am, order_list):
+    cost = 0
+    for i in range(len(order_list) - 1):
+        new_way = find_path(ancestors, order_list[i], order_list[i + 1], [])
+        if new_way == "umulig":
+            return new_way
+        cost += sum([am[new_way[j]][new_way[j + 1]] for j in range(len(new_way) - 1)])
+    return cost
+
+
+def find_path(ancestors, i, j, way):
+    if i == j:
+        way.append(j)
+        return way
+    elif ancestors[i][j] is None:
+        return "umulig"
+    else:
+        new_way = find_path(ancestors, i, ancestors[i][j], way) + [j]
+        return new_way
+
+
+#stdin = open("input_eksempel_01.txt", "r")
+testcases = int(stdin.readline())
+for test in range(testcases):
+    cities = int(stdin.readline())
+    order_list = [int(city) for city in stdin.readline().split()]
+    adjacency_matrix = []
+    for city in range(cities):
+        adjacency_matrix.append([int(w) for w in stdin.readline().split()])
+    print(shortest_path(order_list, adjacency_matrix, cities))
