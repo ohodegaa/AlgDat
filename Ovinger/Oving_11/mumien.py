5df5c5159e279a9fa8b785078797046b4d1dc01a
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-15 09:18:42 +0100

    Implemented Dijkstra

--- Ovinger/Oving_11/mumien.py
+++ Ovinger/Oving_11/mumien.py
@@ -2,8 +2,38 @@ __author__ = 'ohodegaa'
 
 from sys import stdin, stderr
 
-
 def best_path(nm, prob):
+    return dijkstra(nm, prob)
+
+def dijkstra(nm, prob):
+    n = len(nm)
+    visited = [False]*n
+    ancestors = [-1]*n
+    k_prob = [prob[0]] + [0.0]*(n-1)
+    next_node = 0
+    for i in range(n):
+        node = next_node
+        visited[node] = True
+        maxi_prob = -1.0
+        for adj in range(n):
+            if not visited[adj]:
+                if nm[node][adj]*k_prob[node]*prob[adj] > k_prob[adj]:
+                    k_prob[adj] = k_prob[node]*prob[adj]
+                    ancestors[adj] = node
+                    if k_prob[adj] > maxi_prob:
+                        next_node = adj
+                        maxi_prob = k_prob[adj]
+
+        if next_node == n - 1:
+            break
+    anc = n - 1
+    s = str(n - 1)
+    while ancestors[anc] != -1:
+        s = str(ancestors[anc]) + "-" + s
+        anc = ancestors[anc]
+    return s
+
+def floyd_warshall(nm, prob):
     ancestors = []
 
     for i in range(len(nm)):
@@ -33,7 +63,7 @@ def print_path(ancestors, i, j, s):
     else:
         return print_path(ancestors, i, ancestors[i][j], s) + s + "-" + str(j)
 
-
+#stdin = open("input_eksempel_01.txt")
 n = int(stdin.readline())
 probabilities = [float(x) for x in stdin.readline().split()]
 neighbour_matrix = []
